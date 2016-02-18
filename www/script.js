38777f7c5d445adbd5d6214c04003aaa81ada3bb
Flávio Veloso <flaviovs@magnux.com>
2016-02-18 00:55:53 -0200

    Add automatic JS refresh via Vault.config.refresh

--- www/script.js
+++ www/script.js
@@ -2,4 +2,8 @@
 
 	d.getElementsByTagName('html')[0].setAttribute('class', 'js');
 
+	if ( typeof Vault.config.refresh == 'number' ) {
+		setTimeout(d.location.reload, Vault.config.refresh * 1000);
+	}
+
 })(window, document);
