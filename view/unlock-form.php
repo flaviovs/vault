8d8d54562b8827aeeb67657be5eb0553f9a93389
Flávio Veloso <flaviovs@magnux.com>
2016-02-23 21:11:22 -0300

    Improve formatting of the unlock page

--- view/unlock-form.php
+++ view/unlock-form.php
@@ -1,9 +1,13 @@
+<p>Use this form to unlock the information you requested.<p>
+
 <form id="input" action="<?php echo $action ?>" method="POST">
   <input type="hidden" name="reqid" value="<?php echo $reqid ?>">
   <input type="hidden" name="m" value="<?php echo htmlspecialchars($mac) ?>">
 
-  <label for="key">Unlock key</label>
-  <input type="text" name="key" id="key" required>
+  <div id="element-key" class="form-group">
+	<label for="key">Unlock key</label>
+	<input type="text" name="key" id="key" required>
+  </div>
 
-  <input type="submit">
+  <input type="submit" value="Unlock">
 </form>
--- view/unlock-view.php
+++ view/unlock-view.php
@@ -1,5 +1,10 @@
-<p>You have unlocked the secret data:</p>
+<p>You have unlocked the information:</p>
 
 <pre id="unlocked-secret"><?php print $plaintext ?></pre>
 
-<p>Notice that the secret was removed from the Vault, and <b>can not be retrieved again</b>, so <b>copy and paste it into your favorite secure password/data app</b>. Remember, this is sensitive data that belongs to the user, so <b>do not write it down on paper, nor put it in any other insecure media (such as e-mail, chats, unencrypted files, etc.)</p>
+<aside>
+<h2>Notice</h2>
+<ul>
+  <li>The information above was permanently removed from the Vault. <b>You will not be able to see it again</b>. If you think you will need it at a later time, you should <b>copy and paste it into your favorite secure password/data app</b>.</li>
+  <li>Remember, this is sensitive data that belongs to the user, so <b>do not write it down on paper, nor put it in any other insecure media (such as e-mail, chats, unencrypted files, etc.)</li>
+</ul>
