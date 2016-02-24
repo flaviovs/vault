e59b16c560d67188014b8e481784f38bdc399f7e
Flávio Veloso <flaviovs@magnux.com>
2016-02-24 04:05:38 -0300

    Improve 'thank you' text

--- view/input-thank-you.php
+++ view/input-thank-you.php
@@ -1,2 +1,2 @@
-<p>The information you provided was encrypted and locked within our systems.</p>
+<p>The information you provided was encrypted and locked inside our systems.</p>
 <p>The Happiness Engineer was notified of your submission. Only he/she has the key to unlock and decipher the data.</p>
