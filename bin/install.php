501a63f23a2d095070978ed3108ceb508e1b1db7
Flávio Veloso <flaviovs@magnux.com>
2016-03-10 18:03:32 -0300

    Fix return value of installer app controller

--- bin/install.php
+++ bin/install.php
@@ -6,8 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 function main() {
 	$app = new Installer_App( 'install' );
-	$res = $app->run();
-	return 200 == $res ? 0 : intval( $res / 100 );
+	return $app->run();
 }
 
 exit( main() );
